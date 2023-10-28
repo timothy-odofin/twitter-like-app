1331ffd7ecb58e7421b89f7347f8712aaecd52ff
Odofin Oyejide <odofin@swipe.ng>
2023-10-28 09:47:12 +0100

    Initialized springboot project.

---
+++ src/main/java/odofin/oyejide/twitterlikeapp/TwitterLikeAppApplication.java
@@ -0,0 +1,13 @@
+package odofin.oyejide.twitterlikeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterLikeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterLikeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/odofin/oyejide/twitterlikeapp/TwitterLikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package odofin.oyejide.twitterlikeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterLikeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
